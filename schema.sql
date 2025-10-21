e07b93632016f01f98cdeeb862d44e081610e506
jasonclone <Zanack74@gmail.com>
2025-10-21 15:46:27 -0700

    final updates before pushing to main

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 -- schema.sql
--- creates the database 'Comp440MarketplaceWebsite' if it does not exist, then creates the 'Users' table
+-- creates the database 'Comp440SocialNetworkWebsite' if it does not exist, then creates the 'Users' table
 -- import this file on phpMyAdmin
 
-CREATE DATABASE IF NOT EXISTS Comp440MarketplaceWebsite
+CREATE DATABASE IF NOT EXISTS Comp440SocialNetworkWebsite
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_0900_ai_ci;
 
-USE Comp440MarketplaceWebsite; -- switch to the 'Comp440MarketplaceWebsite' database before creating the table
+USE Comp440SocialNetworkWebsite; -- switch to the 'Comp440SocialNetworkWebsite' database before creating the table
 
 CREATE TABLE IF NOT EXISTS Users (
   username VARCHAR(50) PRIMARY KEY,
